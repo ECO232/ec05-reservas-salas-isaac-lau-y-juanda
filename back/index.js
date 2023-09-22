a33a4bd70e99e8368c3cb1ea676232e3ad3a702f
IsaakuTK <isaaccallecardozo@gmail.com>
2023-09-21 19:22:35 -0500

    adding some validations for reservations

--- back/index.js
+++ back/index.js
@@ -2,27 +2,70 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// Use Json middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//const cors = require('cors'); 
-//app.use(cors())
+const cors = require('cors'); 
+app.use(cors())
 
 
 
 const {validateUser} = require('./schemas/users')
 
-// Esto no es REST pero mientras tantillo
-let users = []
-let rooms = []
-let reserva 
+
+let users = [];
+let rooms = [];
+let reservas = [];
+
+
+//rooms 
+let i=1;
+    while(i<3){
+        let newRoom = {
+            name: "room "+ i,
+            id: i,
+            location: "10"+i+"L"
+        }
+        i++
+        rooms.push(newRoom)
+    }
+
+    
+//user
+
 users.push({
-    name: "Pepito",
-    last: "Perez",
-    age: 26,
-    id: "1130613425",
-    email: "pperez@u.icesi.edu.co"
+    nombre: "Bubu",
+    apellido: "Valencia",
+    id: "1013463590",
+})
+
+
+
+//reserva
+reservas.push({
+    id_user: "1013463590",
+    id_room: "1",
+    hour: "2:00",
+    id: "1"
+})
+
+
+//gets
+app.get('/', (req, res)=>{
+    res.send("Bienvenidos a la API de usuarios")
+})
+
+app.get('/rooms', (req, res)=>{
+    res.send({"rooms":rooms})
+})
+
+app.get('/users', (req, res)=>{
+    res.send({"users":users})
+})
+
+app.get('/reservas', (req, res)=>{
+    res.send({"users":users})
 })
 
 app.get('/users/:id', (req, res)=>{
@@ -40,15 +83,8 @@ app.get('/users/:id', (req, res)=>{
 })
 
 
-app.get('/users', (req, res)=>{
-    if(req.query.age){
-        users = users.filter(
-            (user)=>{return user.age == req.query.age}
-        )
-    }
-    res.send({"users":users})
-})
 
+//post
 app.post('/users', (req, res) => {
 
     const userValidationResult = validateUser(req.body)    
@@ -71,18 +107,47 @@ app.post('/users', (req, res) => {
     res.status(201).send({"message":"Creación Exitosa!", "user":newUser})
 })
 
-app.get('/', (req, res)=>{
-    res.send("Bienvenidos a la API de usuarios")
-})
 
+//delete
 app.delete('/users/:id', (req, res)=>{
     const idToDelete = req.params.id;
     let indexToDelete = users.findIndex(user=>user.id==idToDelete)
     let userDeleted = users.splice(indexToDelete, 1)
-    //console.log("user delete: ", userDeleted)
+
     res.send("Se eliminó correctamente el usuario con id: " + userDeleted[0].id)
+    borrarreserva(userDeleted);
 })
 
+
+app.delete('/reservas/:id', (req, res)=>{
+    const idToDelete = req.params.id;
+    let indexToDelete = users.findIndex(user=>user.id==idToDelete)
+    let reservaDeleted = users.splice(indexToDelete, 1)
+
+    res.send("Se eliminó correctamente reservas con id: " + reservaDeleted[0].id)
+})
+
+app.delete('/rooms/:id', (req, res)=>{
+    const idToDelete = req.params.id;
+    let indexToDelete = rooms.findIndex(user=>user.id==idToDelete)
+    let roomsDeleted = rooms.splice(indexToDelete, 1)
+
+    res.send("Se eliminó correctamente rooms con id: " + roomsDeleted[0].id)
+})
+
+function borrarreserva(userDeleted){
+    app.delete('/reservas/:id_user', (req, res)=>{
+        const idToDelete = req.params.id;
+        let indexToDelete = reservas.findIndex(reserva=>reserva.id==idToDelete)
+        let reservaDeleted = reservas.splice(indexToDelete, 1)
+    
+        res.send("Se eliminó correctamente el usuario y la sala: " + reservaDeleted[0].id)
+    })
+}
+
+
+
+//put
 app.put('/users/:id',(req, res)=>{
     let index = users.findIndex(user => user.id == req.params.id)
     let newUser = {
@@ -96,20 +161,15 @@ app.put('/users/:id',(req, res)=>{
     res.send("usuario reemplazado " + newUser )
 })
 
+
+//patch
 app.patch('/users/:id', (req, res)=>{
     let index = users.findIndex(user => user.id == req.params.id)
 
-    /* A pie 🦶*/
-    //users[index].name = req.body.name || users[index].name
-    //users[index].last = req.body.last || users[index].last
-    //users[index].age = req.body.age || users[index].age
-    //users[index].email = req.body.email || users[index].email
-
-    /* Para generalizarlo ⭐*/
     if (index !== -1) {
-        // Obtén las claves del cuerpo de la solicitud
+
         const requestKeys = Object.keys(req.body);
-        // Itera sobre las claves y verifica si existen en el objeto
+
         requestKeys.forEach(key => {
             if (users[index][key] !== undefined) {
                 users[index][key] = req.body[key];
@@ -120,6 +180,9 @@ app.patch('/users/:id', (req, res)=>{
         res.status(404).send("Usuario no encontrado");
     }
 });
+
+
+
 
 app.use("", (req, res)=>{
     res.status(404).send("No encontramos el recurso solicitado")
